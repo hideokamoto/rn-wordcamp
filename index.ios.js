be71ed58646f731e4332ff48679e69b2a3a0fe4d
hideokamoto <hide.okamoto@digitalcube.jp>
2016-04-15 00:28:41 +0900

    add tab bar

--- index.ios.js
+++ index.ios.js
@@ -4,15 +4,110 @@ import React, {
   StyleSheet,
   Text,
   View,
+	TabBarIOS,
   ScrollView
 } from 'react-native';
+var Icon = require('react-native-vector-icons/MaterialIcons');
 import {
 	Toolbar as MaterialToolbar,
 } from 'react-native-material-design';
 
 var CampList = require('./src/component/CampList.js');
 
+class NewsList extends React.Component {
+	constructor( props ) {
+		super( props );
+	}
+
+	render() {
+		return (
+			<View>
+				<Text>News</Text>
+			</View>
+		)
+	}
+}
+
 class WckApp extends React.Component {
+	constructor( props ) {
+		super( props );
+		this.state = {
+			selectedTab: "news"
+		};
+	}
+
+	_renderContent() {
+		if ( 'news' === this.state.selectedTab ) {
+			return (
+				<NewsList />
+			)
+		}
+		return (
+			<View>
+				<Text>aa</Text>
+			</View>
+		)
+	}
+
+	render() {
+		return (
+			<TabBarIOS
+				tintColor="white"
+				barTintColor="darkslateblue">
+				<Icon.TabBarItem
+					title="news"
+					iconName='home'
+					selected={ this.state.selectedTab === 'news' }
+					onPress={() => {
+						this.setState({
+							selectedTab: 'news',
+						})
+					}}
+				>
+					{this._renderContent()}
+				</Icon.TabBarItem>
+				<Icon.TabBarItem
+					title="session"
+					iconName='speaker-notes'
+					selected={ this.state.selectedTab === 'session' }
+					onPress={() => {
+						this.setState({
+							selectedTab: 'session',
+						})
+					}}
+				>
+					{this._renderContent()}
+				</Icon.TabBarItem>
+				<Icon.TabBarItem
+					title="speaker"
+					iconName='supervisor-account'
+					selected={ this.state.selectedTab === 'speaker' }
+					onPress={() => {
+						this.setState({
+							selectedTab: 'speaker',
+						})
+					}}
+				>
+					{this._renderContent()}
+				</Icon.TabBarItem>
+				<Icon.TabBarItem
+					title="sponsor"
+					iconName='redeem'
+					selected={ this.state.selectedTab === 'sponsor' }
+					onPress={() => {
+						this.setState({
+							selectedTab: 'sponsor',
+						})
+					}}
+				>
+					{this._renderContent()}
+				</Icon.TabBarItem>
+			</TabBarIOS>
+		)
+	}
+}
+
+class WckAppInner extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
