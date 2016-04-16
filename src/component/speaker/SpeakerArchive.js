31c4739097086db296eaa484c373b4836b2838b3
hideokamoto <hide.okamoto@digitalcube.jp>
2016-04-16 15:43:11 +0900

    file copy for speaker

---
+++ src/component/speaker/SpeakerArchive.js
@@ -0,0 +1,77 @@
+import React, {
+	Component,
+	StyleSheet,
+	Text,
+	View,
+	ListView,
+	ScrollView
+} from 'react-native';
+import {
+	Toolbar as MaterialToolbar,
+	Card
+} from 'react-native-material-design';
+var SessionExcerpt = require('./SessionExcerpt.js');
+
+class SessionArchive extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			SessionArchive: new ListView.DataSource({
+				rowHasChanged: (row1, row2) => row1 !== row2,
+			}),
+			isLoaded: false
+		};
+	}
+
+	renderLoadingView() {
+		return (
+			<View>
+				<Text>Loading...</Text>
+			</View>
+		)
+	}
+
+	renderSessionArchiveItem( item, sectionID, rowID ) {
+		return (
+			<SessionExcerpt item={item} />
+		);
+	}
+
+	renderSessionArchiveView() {
+		return (
+			<ScrollView style={{ marginTop: 60}}>
+				<ListView
+					dataSource={this.state.SessionArchive}
+					renderRow={this.renderSessionArchiveItem}
+					/>
+			</ScrollView>
+		);
+	}
+
+	fetchCentralApi() {
+		fetch( this.props.apiPath )
+			.then( ( response ) => response.json())
+			.then( ( responseData ) => {
+				console.log(responseData);
+				this.setState({
+					SessionArchive: this.state.SessionArchive.cloneWithRows( responseData ),
+					isLoaded: true,
+				});
+			})
+			.done();
+	}
+
+	componentDidMount() {
+		this.fetchCentralApi();
+	}
+
+	render() {
+		if ( this.state.isLoaded ) {
+			return this.renderSessionArchiveView();
+		} else {
+			return this.renderLoadingView();
+		}
+	}
+}
+
+module.exports = SessionArchive;
